a15ca4b618fb13316e302bed0a1d820fc6f2e248
Jody Bailey <jody@jodys-mbp>
2017-12-16 00:39:43 -0600

    Initial Commit

---
+++ BodyCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyCalc
+//
+//  Created by Jody Bailey on 12/16/17.
+//  Copyright © 2017 Jody Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
